69d1c59e17f1a613ac66ef26e8b29db5680af92c
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2024-01-11 21:55:52 +0545

    Change to new flow

--- caching/cache/cache.go
+++ caching/cache/cache.go
@@ -1,59 +1,177 @@
 package cache
 
 import (
-	"fmt"
-	"sync"
+	"errors"
 	"time"
 )
 
 type Cache struct {
-	lock sync.RWMutex
-	data map[string][]byte
+	cacheMap map[string]*Node
+	queue    *Queue
 }
 
+type Queue struct {
+	head *Node
+	tail *Node
+}
+
+type Node struct {
+	data interface{}
+	prev *Node
+	next *Node
+}
+
+// NewCache creates a new Cache with an empty map and queue.
 func New() *Cache {
 	return &Cache{
-		data: make(map[string][]byte),
+		cacheMap: make(map[string]*Node),
+		queue:    NewQueue(),
 	}
 }
 
-func (c *Cache) Get(key []byte) ([]byte, error) {
-	c.lock.RLock()
-	defer c.lock.RUnlock()
-
-	val, ok := c.data[string(key)]
-	if !ok {
-		return nil, fmt.Errorf("searched key %s not found", string(key))
-	}
-	return val, nil
+// NewQueue creates a new Queue with empty head and tail.
+func NewQueue() *Queue {
+	return &Queue{}
 }
 
-func (c *Cache) Set(key []byte, value []byte, ttl time.Duration) error {
-	c.lock.Lock()
-	defer c.lock.Unlock()
+// AddToFront adds a new node to the front of the queue.
+func (q *Queue) AddToFront(node *Node) {
+	node.next = q.head
+	if q.head != nil {
+		q.head.prev = node
+	}
+	q.head = node
+	if q.tail == nil {
+		q.tail = node
+	}
+}
 
-	c.data[string(key)] = value
+// MoveToFront moves the given node to the front of the queue.
+func (q *Queue) MoveToFront(node *Node) {
+	if node == nil {
+		return
+	}
+
+	// Remove the node from its current position
+	if nodePrev := node.prev; nodePrev != nil {
+		nodePrev.next = node.next
+	} else {
+		q.head = node.next
+	}
+
+	if nodeNext := node.next; nodeNext != nil {
+		nodeNext.prev = node.prev
+	} else {
+		q.tail = node.prev
+	}
+
+	// Add the node to the front
+	q.AddToFront(node)
+}
+
+// RemoveNode removes the given node from the queue.
+func (q *Queue) RemoveNode(node *Node) {
+	if node == nil {
+		return
+	}
+
+	// Remove the node from its current position
+	if nodePrev := node.prev; nodePrev != nil {
+		nodePrev.next = node.next
+	} else {
+		q.head = node.next
+	}
+
+	if nodeNext := node.next; nodeNext != nil {
+		nodeNext.prev = node.prev
+	} else {
+		q.tail = node.prev
+	}
+}
+
+// RemoveFromEnd removes the last node from the queue.
+func (q *Queue) RemoveFromEnd() *Node {
+	if q.tail == nil {
+		return nil
+	}
+
+	removedNode := q.tail
+	q.tail = q.tail.prev
+
+	if q.tail != nil {
+		q.tail.next = nil
+	} else {
+		q.head = nil
+	}
+
+	return removedNode
+}
+
+func (c *Cache) Get(key string) ([]byte, error) {
+	node, exists := c.cacheMap[key]
+	if !exists {
+		return nil, errors.New("key not found")
+	}
+
+	// Move the accessed node to the front of the queue
+	c.queue.MoveToFront(node)
+
+	return node.data.([]byte), nil
+}
+
+func (c *Cache) Set(key string, value []byte, duration time.Duration) error {
+	node, exists := c.cacheMap[key]
+	if exists {
+		// Update existing node
+		node.data = value
+		c.queue.MoveToFront(node)
+	} else {
+		// Add new node
+		newNode := &Node{
+			data: value,
+			next: c.queue.head,
+			prev: c.queue.tail,
+		}
+		c.cacheMap[key] = newNode
+		c.queue.AddToFront(newNode)
+	}
 
 	go func() {
-		<-time.After(ttl)
-		delete(c.data, string(key))
+		<-time.After(duration)
+		delete(c.cacheMap, key)
 	}()
+	return nil
+}
+
+func (c *Cache) Has(key string) bool {
+	_, exists := c.cacheMap[key]
+	return exists
+}
+
+func (c *Cache) Delete(key string) error {
+	node, exists := c.cacheMap[key]
+	if !exists {
+		return errors.New("key not found")
+	}
+
+	c.queue.RemoveNode(node)
+	delete(c.cacheMap, key)
 
 	return nil
 }
 
-func (c *Cache) Has(key []byte) bool {
-	c.lock.RLock()
-	defer c.lock.RUnlock()
-
-	_, ok := c.data[string(key)]
-	return ok
+// ResetCache resets the cache by clearing the map and queue.
+func (c *Cache) ResetCache() error {
+	c.cacheMap = make(map[string]*Node)
+	c.queue = NewQueue()
+	return nil
 }
 
-func (c *Cache) Delete(key []byte) error {
-	c.lock.Lock()
-	defer c.lock.Unlock()
+func (c *Cache) IsFull() bool {
+	// TODO: Implement logic to check if the cache is full
+	return false
+}
 
-	delete(c.data, string(key))
-	return nil
+func (c *Cache) IsEmpty() bool {
+	return len(c.cacheMap) == 0
 }
