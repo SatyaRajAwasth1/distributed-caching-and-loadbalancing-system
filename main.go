133ebeef8b8dccefbfd070e3eefbd31b08a1e2a4
Satya Raj Awasthi <77236280+SatyaRajAwasth1@users.noreply.github.com>
2024-02-22 01:05:23 +0545

    Revert "Fix: Fixes on aof persistence and showing cache contents"

--- main.go
+++ main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"distributed-caching-and-loadbalancing-system/caching/cache"
+	"distributed-caching-and-loadbalancing-system/caching/server"
 	"fmt"
+	"log"
+	"net"
+	"time"
 )
 
 func main() {
-	fmt.Println("\n````````````````````````````````````````````````````````````````")
+	fmt.Println("Its just a start of distributed cache and load balancing system")
 
-	// Handle CLI operations
-	cache.HandleCli()
+	serverOperations := server.Operations{
+		ListenAddress: ":8080",
+		IsLeader:      true,
+	}
 
-	fmt.Println("````````````````````````````````````````````````````````````````")
+	go func() {
+		time.Sleep(time.Second * 2)
+		conn, err := net.Dial("tcp", ":8080")
+		if err != nil {
+			return
+		}
+
+		_, _ = conn.Write([]byte("Hello, Server From Satya"))
+	}()
+
+	newServer := server.NewServer(serverOperations, cache.New())
+	err := newServer.Start()
+	if err != nil {
+		log.Printf("Error starting server: %s", err)
+	}
 }
